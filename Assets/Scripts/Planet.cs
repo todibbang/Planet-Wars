6e8f6e0b0e0165ec671f8e3c8249f0ccdb403c7c
todibbang <macbookpro@Macbooks-MacBook-Pro.local>
2017-02-02 19:09:45 +0100

    4

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+
+	public float Radius;
+	public float DistanceFromSun;
+	public float YearDuration;
+	public GameObject Sun;
+	float angle = 0f;
+
+	// Use this for initialization
+	void Start () {
+		transform.localScale = new Vector2 (Radius, Radius);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		float x = Sun.transform.position.x + DistanceFromSun * Mathf.Cos(Mathf.Deg2Rad*angle);
+		float y = Sun.transform.position.y + DistanceFromSun * Mathf.Sin(Mathf.Deg2Rad*angle);
+		angle += (Time.deltaTime * 360) / YearDuration;
+		if (angle >= 360)
+			angle = 0;
+		transform.position = new Vector2 (x, y);
+
+
+	}
+}
